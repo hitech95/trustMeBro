1184b2186c10d4f65456b3de70a6bf981b2b66ea
hitech95 <nicveronese@gmail.com>
2023-12-14 15:06:52 +0100

    server: enable new UserService and UserController
    This partially provides CRUD/BREAD for the User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './providers/prisma.service';
+import { UserService } from './modules/user/user.service';
+import { UserController } from './modules/user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [PrismaService, AppService],
+  controllers: [AppController, UserController],
+  providers: [PrismaService, AppService, UserService],
 })
 export class AppModule {}
